3a7de3b1ccf2ef4338ad0e897fb5769789d07347
Sebastian Quintana <sebaa08@gmail.com>
2026-02-28 11:38:33 -0300

    chore: prepare for production deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
   });
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
